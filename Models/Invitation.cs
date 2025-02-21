0c8f15eaa36641656e06d151f390b318600c5fb0
BBualdo <mrseb10@gmail.com>
2025-02-21 18:14:03 +0100

    Added Invitation Model

---
+++ Models/Invitation.cs
@@ -0,0 +1,13 @@
+﻿namespace Models;
+
+public class Invitation
+{
+    public Guid Id { get; set; }
+    public DateTime ExpirationDate { get; set; }
+    public required string InvitedUserId { get; set; }
+    public User? InvitedUser { get; set; }
+    public Guid HouseHoldId { get; set; }
+    public Household? Household { get; set; }
+    public required string InvitingUserId { get; set; }
+    public User? InvitingUser { get; set; }
+}
