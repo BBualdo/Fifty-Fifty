d79c93b50e13bb8ed86d86857de95257ffe8df20
BBualdo <mrseb10@gmail.com>
2025-02-28 17:23:08 +0100

    JWT Token generating implementation using data from options model configured with appsettings.json

--- Services/JwtService.cs
+++ Services/JwtService.cs
@@ -1,5 +1,10 @@
-﻿using DTOs;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using DTOs;
+using Microsoft.IdentityModel.Tokens;
 using Models;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
 namespace Services;
 
@@ -9,7 +14,26 @@ public class JwtService(JwtSettings jwtSettings) : IJwtService
     
     public string GenerateToken(User user)
     {
-        throw new NotImplementedException();
+        var claims = new List<Claim>
+        {
+            new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new(JwtRegisteredClaimNames.Email, user.Email),
+            new(JwtRegisteredClaimNames.Name, user.Username),
+            new("role", user.Role.ToString()),
+        };
+        
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Secret));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            claims,
+            expires: DateTime.Now.AddMinutes(_jwtSettings.ExpirationMinutes),
+            signingCredentials: credentials
+            );
+        
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 
     public RefreshToken GenerateRefreshToken()
--- Tests/Services/JwtServiceTests.cs
+++ Tests/Services/JwtServiceTests.cs
@@ -1,4 +1,6 @@
-﻿using DTOs;
+﻿using System.IdentityModel.Tokens.Jwt;
+using DTOs;
+using Models;
 using Services;
 
 namespace Tests.Services;
@@ -19,5 +21,35 @@ public class JwtServiceTests
         };
 
         _jwtService = new JwtService(_jwtSettings);
+    }
+
+    [Fact]
+    public void GenerateToken_ShouldReturnValidJwtToken()
+    {
+        // Arrange
+        var user = new User()
+        {
+            Id = Guid.NewGuid(),
+            Email = "test@email.com",
+            Username = "TestUser",
+            Role = UserRole.User
+        };
+        
+        // Act
+        var token = _jwtService.GenerateToken(user);
+
+        // Assert
+        Assert.NotNull(token);
+        Assert.False(string.IsNullOrEmpty(token));
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var jwtToken = tokenHandler.ReadJwtToken(token);
+        
+        Assert.Equal(jwtToken.Issuer, _jwtSettings.Issuer);
+        Assert.Equal(jwtToken.Audiences.First(), _jwtSettings.Audience);
+        Assert.Equal(jwtToken.Claims.First(c => c.Type == "sub").Value, user.Id.ToString());
+        Assert.Equal(jwtToken.Claims.First(c => c.Type == "email").Value, user.Email);
+        Assert.Equal(jwtToken.Claims.First(c => c.Type == "role").Value, user.Role.ToString());
+        Assert.Equal(jwtToken.Claims.First(c => c.Type == "name").Value, user.Username);
     }
 }
